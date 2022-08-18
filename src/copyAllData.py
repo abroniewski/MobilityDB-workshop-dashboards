82deeb264232eb0dd2b4ed0bb74d6b91d2811718
abroniewski <abroniewski@gmail.com>
2022-08-18 15:43:50 +0200

    added try except around database connection.

--- src/copyAllData.py
+++ src/copyAllData.py
@@ -8,21 +8,27 @@ print("Opened database successfully")
 cur = conn.cursor()
 path = "/Users/adambroniewski/DATA for Projects/OpenSky Data"
 
+try:
+    for directory in os.listdir(path):
+        if directory.endswith(".csv"):
+            directory_path = os.path.join(path,directory)
+            for fname in os.listdir(directory_path):
+                if fname.endswith(".csv"):
+                    fname_path = os.path.join(directory_path,fname)
+                    copy_command = f'''
+                        COPY flights(et, icao24, lat, lon, velocity, heading, vertrate, callsign, onground, alert, spi, squawk, baroaltitude, geoaltitude, lastposupdate, lastcontact)
+                        FROM '{fname_path}' DELIMITER  ',' CSV HEADER;'''
 
-for directory in os.listdir(path):
-    if directory.endswith(".csv"):
-        directory_path = os.path.join(path,directory)
-        for fname in os.listdir(directory_path):
-            if fname.endswith(".csv"):
-                fname_path = os.path.join(directory_path,fname)
-                copy_command = f'''
-                    COPY flights(et, icao24, lat, lon, velocity, heading, vertrate, callsign, onground, alert, spi, squawk, baroaltitude, geoaltitude, lastposupdate, lastcontact)
-                    FROM '{fname_path}' DELIMITER  ',' CSV HEADER;'''
 
+                    cur.execute(copy_command)
+                    conn.commit()
+                    print(f"Added csv {fname}")
+    print("Data import complete!")
 
-                cur.execute(copy_command)
-                conn.commit()
-                print(f"Added csv from {copy_command}")
+except Exception as e:
+    print("There was an error!")
+    print(e)
 
-conn.close()
-print("Data import complete!")
+finally:
+    conn.close()
+    print("Connection closed")
